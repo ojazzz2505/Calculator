b0e4a281c00bc1c4527dde88e3e7c8b473477bd7
Ojas Trivedi <91268238+ojazzz2505@users.noreply.github.com>
2024-08-26 15:11:09 -0400

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number:");
+            // Convert the input string to a decimal
+            decimal FirstNum = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter the second number:");
+            // Convert the input string to a decimal
+            decimal SecondNum = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Addition - a\nSubtraction - s\nMultiplication - m\nDivision - d\nModulus - M");
+
+            Console.WriteLine("------------------------");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "a")
+            {
+                Console.WriteLine("Result: " + (FirstNum + SecondNum));
+            }
+            else if (choice == "s")
+            {
+                Console.WriteLine("Result: " + (FirstNum - SecondNum));
+            }
+            else if (choice == "m")
+            {
+                Console.WriteLine("Result: " + (FirstNum * SecondNum));
+            }
+            else if (choice == "d")
+            {
+                if (SecondNum != 0)
+                {
+                    Console.WriteLine("Result: " + (FirstNum / SecondNum));
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero");
+                }
+            }
+            else if (choice == "M")
+            {
+                if (SecondNum != 0)
+                {
+                    Console.WriteLine("Result: " + (FirstNum % SecondNum));
+                }
+                else
+                {
+                    Console.WriteLine("Error: Modulus by zero.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Choice was invalid!");
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
